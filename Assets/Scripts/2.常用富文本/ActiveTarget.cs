5d15068576aafd25c42b006fa85fc381eece355d
su9257 <su9257@outlook.com>
2019-08-15 02:21:26 +0800

    更新

--- Assets/Scripts/2.常用富文本/ActiveTarget.cs
+++ Assets/Scripts/2.常用富文本/ActiveTarget.cs
@@ -10,19 +10,11 @@ public class ActiveTarget : MonoBehaviour
 
     void Start()
     {
-       GetComponent<Button>().onClick.AddListener(()=> 
+       GetComponent<Button>().onClick.AddListener(()=>
         {
-            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
-            for (int i = 0; i < gameObjects.Length; i++)
+            if (targetObject != null)
             {
-                if (targetObject.name == gameObjects[i].name)
-                {
-                    gameObjects[i].SetActive(true);
-                }
-                else
-                {
-                    gameObjects[i].SetActive(false);
-                }
+                TextGroup.Instance.OpenObject(targetObject.name);
             }
         });
     }
---
+++ Assets/Scripts/2.常用富文本/TextGroup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextGroup : MonoBehaviour
+{
+    public List<TextMeshProUGUI> textMeshProUGUIs = new List<TextMeshProUGUI>();
+    private static TextGroup instance = null;
+    public static TextGroup Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            textMeshProUGUIs.Add(transform.GetChild(i).GetComponent<TextMeshProUGUI>());
+        }
+    }
+
+    public void OpenObject(string Name)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (textMeshProUGUIs[i].name == Name)
+            {
+                textMeshProUGUIs[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                textMeshProUGUIs[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
