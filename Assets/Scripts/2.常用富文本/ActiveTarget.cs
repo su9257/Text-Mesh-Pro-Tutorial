010712cfe374bc98a65b77397d9e64e0713242bf
su9257 <su9257@outlook.com>
2019-08-15 00:32:57 +0800

    更新

---
+++ Assets/Scripts/2.常用富文本/ActiveTarget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class ActiveTarget : MonoBehaviour
+{
+
+    public GameObject targetObject = null;
+
+    void Start()
+    {
+       GetComponent<Button>().onClick.AddListener(()=> 
+        {
+            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
+            for (int i = 0; i < gameObjects.Length; i++)
+            {
+                if (targetObject.name == gameObjects[i].name)
+                {
+                    gameObjects[i].SetActive(true);
+                }
+                else
+                {
+                    gameObjects[i].SetActive(false);
+                }
+            }
+        });
+    }
+
+
+}
